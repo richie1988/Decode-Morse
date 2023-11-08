aa0dab4b9568b004e298e4649de8403041f696e0
richie1988 <richiessikaonga@gmail.com>
2023-11-08 18:36:08 +0200

    First ruby morse code approach

--- decode_morse_msg.rb
+++ decode_morse_msg.rb
@@ -1,29 +1,29 @@
-MORSE_CODE ={
-    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
-    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
-    '-.-' => 'K','.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
-    '.--.' => 'P','--.-' => 'Q', '.-.' => 'R', '...' => 'S','-' => 'T',
-    '..-' => 'U','...-' => 'V', '.--' => 'W', '-..-' => 'X','-.--' => 'Y',
-    '--..' => 'Z',
-    ' ' => ' '
-}
+MORSE_CODE = {
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+  '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+  '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+  '--..' => 'Z',
+  ' ' => ' '
+}.freeze
 
 def decode_char(morse_char)
-    MORSE_CODE[morse_char] || "" # Return the corresponding character or an empty string if not found
-  end
-  
-  # Method to decode a whole word in Morse code
-  def decode_word(morse_word)
-    morse_word.split(" ").map { |morse_char| decode_char(morse_char) }.join
-  end
-  
-  # Method to decode an entire message in Morse code
-  def decode_message(morse_message)
-    morse_message.split("   ").map { |morse_word| decode_word(morse_word) }.join(" ")
-  end
-  
-  # Morse code message from the old bottle
-  message = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
-  decoded_message = decode_message(message)
-  
-  puts decoded_message
+  MORSE_CODE[morse_char] || '' # Return the corresponding character or an empty string if not found
+end
+
+# Method to decode a whole word in Morse code
+def decode_word(morse_word)
+  morse_word.split.map { |morse_char| decode_char(morse_char) }.join
+end
+
+# Method to decode an entire message in Morse code
+def decode_message(morse_message)
+  morse_message.split('   ').map { |morse_word| decode_word(morse_word) }.join(' ')
+end
+
+# Morse code message from the old bottle
+message = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+decoded_message = decode_message(message)
+
+puts decoded_message
